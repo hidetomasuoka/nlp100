956c6322e167b5531a31e2cd6cadca8b0173d350
hidetomasuoka <hidemasuoka112@gmail.com>
2016-03-11 19:50:31 +0900

    pro version

--- 1/knock003.py
+++ 1/knock003.py
@@ -1,11 +1,9 @@
 # coding: utf-8
 # -*- coding: utf-8 -*-
 
+import re
+
 s = u"Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
-s2 = []
-words = s.split(" ")
 
-for word in words:
-	s2.append(len(word))
-
-print(s2)
+pat = re.compile('[.,]')
+print([len(pat.sub('', word)) for word in s.split()])
